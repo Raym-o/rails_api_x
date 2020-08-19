410b953ad3568e4af90b14d0548ecad717772100
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-19 01:15:15 -0500

    Added Products with attached images to seed script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+require 'uri'
+require 'json'
+require 'pry'
+require 'net/http'
+
 count = 1
 addr_count = true
 
@@ -53,6 +58,72 @@ USER_COUNT.times do
 
   count += 1
 end
+
+# file = File.open('../../jtg_data.json')
+# data = JSON.load file
+
+# data.each do |dat|
+#   # container for image details to loop through attaching images at the end
+#   a_temp_images = []
+
+#   p = Product.new(
+#     title: dat['title'],
+#     price: dat['price'],
+#     description: dat['description']['main_description']
+#   )
+#   # img_url = 'https:' + img.slice(img.index("'") + 1, (img.rindex("'") - img.index("'") - 1))
+#   dat['image'].each do |_k, v|
+#     ct = 0
+#     temp_image = {}
+#     v.each do |kk, vv|
+#       if kk == 'src'
+#         image_url = 'https:' + vv
+#         temp_image[:url] = image_url
+#         # p.images.attach(io: URI.open(image_url))
+#       end
+#       next unless kk == 'alt'
+
+#       temp_image[:alt] = vv + ct.to_s
+#       ct += 1
+#       next if temp_image[:url].nil?
+
+#       input_image = {}
+#       input_image[:url] = temp_image[:url]
+#       input_image[:alt] = temp_image[:url]
+#       a_temp_images.push(input_image)
+
+#       temp_image[:url] = nil
+#       temp_image[:alt] = nil
+#     end
+#   end
+#   puts a_temp_images.count
+#   a_temp_images.each do |image|
+#     puts '...'
+#     puts image[:url]
+#     puts ',,,'
+
+#     my_url = URI.parse(image[:url])
+#     req = Net::HTTP.new(my_url.host, my_url.port)
+#     req.use_ssl = true
+#     res = req.request_head(my_url.path)
+
+#     puts 'content-type'
+#     puts res['content-type']
+#     puts 'content_type'
+#     if res['content-type'] == 'image/jpeg'
+#       p.images.attach(
+#         io: URI.open(image[:url]),
+#         filename: image[:alt],
+#         content_type: 'image/jpg',
+#         identify: false
+#       )
+#     end
+#     puts 'notnull'
+
+#     p.save!
+#     puts 'SAVING'
+#   end
+# end
 
 if Rails.env.development?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
